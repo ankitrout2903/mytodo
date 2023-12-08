819811cc29f0eefbc1897dbee03a23a571a92d85
ankitrout2903 <88599131+ankitrout2903@users.noreply.github.com>
2023-12-08 19:53:52 +0530

    Initial commit

---
+++ todo/models.py
@@ -0,0 +1,22 @@
+# todo_app/models.py
+from django.db import models
+
+class TodoItem(models.Model):
+    timestamp = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=100)
+    description = models.TextField(max_length=1000)
+    due_date = models.DateTimeField(null=True, blank=True)
+    tags = models.ManyToManyField('Tag', blank=True)
+    status = models.CharField(
+        max_length=10,
+        choices=[
+            ('OPEN', 'Open'),
+            ('WORKING', 'Working'),
+            ('DONE', 'Done'),
+            ('OVERDUE', 'Overdue'),
+        ],
+        default='OPEN',
+    )
+
+class Tag(models.Model):
+    value = models.CharField(max_length=100, unique=True)
